c7028e26a5897a78b0ef5775571c337960c7e283
desilva-se19047 <desilva-se19047@stu.kln.ac.lk>
2023-04-01 18:14:02 +0530

    #

--- src/header.js
+++ src/header.js
@@ -3,7 +3,7 @@ function Header() {
     return(
         <nav class="bg-dark navbar-dark navbar">
             <div className="row col-12 d-flex justify-content-center text-white">
-                <h3>Registration</h3>
+                <h3>Signup</h3>
             </div>
         </nav>
     )
