bd18445f8fd2ff79276e9326f403d9e1ae7932d8
Ravindu Kavinda <90096265+RavinduKavinda@users.noreply.github.com>
2023-04-02 09:09:41 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Cart from './pages/Cart';
+import Home from './pages/Home'
+import Login from './pages/Login';
+import Product from './pages/Product';
+import ProductList from './pages/ProductList';
+import Register from './pages/Register';
+
+const App = () => {
+  return (
+    <Cart/>
+  )
+};
+
+export default App
+
