5e751ce9844e110f71e9e6b24100949040828119
desilva-se19047 <desilva-se19047@stu.kln.ac.lk>
2023-04-03 20:38:45 +0530

    change button name

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -20,7 +20,7 @@ export const Signup = (props) => {
                 <input value={email} onChange={(e) => setEmail(e.target.value)} type= "email"  placeholder="youremail@gmail.com" id="email" name="email"/>
                 <label htmlFor= "password"> password </label>
                 <input value={pass} onChange={(e) => setPass(e.target.value)} type= "password"  placeholder="********" id="password" name="password"/>
-                <button type="submit"> Signin </button>
+                <button type="submit"> <b> Register </b> </button>
             </from>
             <button className="link-btn" onClick={() => props.onFormSwitch('signin')}>Already do you have an account? Signin here.</button>
         </div>
