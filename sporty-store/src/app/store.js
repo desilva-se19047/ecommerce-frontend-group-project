bd4268afb94a2664370429a570b04a2e42352603
Rajeeth07 <rajeeth-se19029@stu.kln.ac.lk>
2023-04-05 01:27:25 +0530

    Admin Panel page

---
+++ sporty-store/src/app/store.js
@@ -0,0 +1,21 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "../features/auth/authSlice";
+import customerReducer from "../features/cutomers/customerSlice";
+import productReducer from "../features/product/productSlice";
+import brandReducer from "../features/brand/brandSlice";
+import pCategoryReducer from "../features/pcategory/pcategorySlice";
+import bCategoryReducer from "../features/bcategory/bcategorySlice";
+import colorReducer from "../features/color/colorSlice";
+import uploadReducer from "../features/upload/uploadSlice";
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    customer: customerReducer,
+    product: productReducer,
+    brand: brandReducer,
+    pCategory: pCategoryReducer,
+    bCategory: bCategoryReducer,
+    color: colorReducer,
+    upload: uploadReducer,
+  },
+});
