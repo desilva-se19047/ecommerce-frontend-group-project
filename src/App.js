c3e8284b35ce28e6c1d03510e1a035de4d2b5bf2
RavinduKavinda <dlravindukavinda@gmail.com>
2023-04-05 21:36:21 +0530

    import react router to app js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import Cart from './pages/Cart';
 import Home from './pages/Home'
-
 import Product from './pages/Product';
 import ProductList from './pages/ProductList';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 
 const App = () => {
   return (
-    <Product/>
+    <Home/>
   )
 };
 
