3aa8d7358f4f6712f72088c0c03c9afb79e59e36
desilva-se19047 <desilva-se19047@stu.kln.ac.lk>
2023-04-05 17:54:17 +0530

    asdd signin signup pages

---
+++ sporty-store/src/pages/Signin.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+export const Signin = (props) => {
+
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email);
+    }
+
+    return (
+        <div className="auth-form-container">
+            <h2>Signin</h2>
+            <from className="signin-form" onSubmit={handleSubmit}>
+                <label htmlFor= "email"> email </label>
+                <input value={email} onChange={(e) => setEmail(e.target.value)} type= "email"  placeholder="youremail@gmail.com" id="email" name="email"/>
+                <label htmlFor= "password"> password </label>
+                <input value={pass} onChange={(e) => setPass(e.target.value)} type= "password"  placeholder="********" id="password" name="password"/>
+                <button type="submit"> <b>Signin</b> </button>
+            </from>
+            <button className="link-btn" onClick={() => props.onFormSwitch('signup')}>Don't you have an account? Register here.</button>
+        </div>
+    )
+
+}
