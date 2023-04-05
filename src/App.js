1c9dd9e57a5e2104a33437f378a158ea8e4e352c
RavinduKavinda <dlravindukavinda@gmail.com>
2023-04-05 21:01:17 +0530

    test

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Cart from './pages/Cart';
+import Home from './pages/Home'
+
+import Product from './pages/Product';
+import ProductList from './pages/ProductList';
+
+
+const App = () => {
+  return (
+    <Product/>
+  )
+};
+
+export default App
+
