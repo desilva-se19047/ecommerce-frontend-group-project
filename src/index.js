b6c381bedbe281dda43b13a8eb05c0a60f0dbea6
RavinduKavinda <dlravindukavinda@gmail.com>
2023-04-06 20:36:51 +0530

    index.js add store commands

--- src/index.js
+++ src/index.js
@@ -13,6 +13,13 @@ root.render(
   </React.StrictMode>
 );
 
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>,
+  document.getElementById('root')
+);
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
