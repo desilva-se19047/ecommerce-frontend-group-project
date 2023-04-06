6c69fcab45f62cc89d3857deddc07f7f4139412f
desilva-se19047 <desilva-se19047@stu.kln.ac.lk>
2023-04-06 19:33:38 +0530

    add lines

--- sporty-store/src/index.js
+++ sporty-store/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import ReactDOM from 'react-dom';
 import "./index.css";
 import App from "./App";
 import reportWebVitals from './reportWebVitals';
@@ -10,6 +11,13 @@ root.render(
   <Provider store={store}>
     <App />
   </Provider>
+);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
