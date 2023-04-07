dc6ec34d999fd2f32e35c44611f9274e3238240c
desilva-se19047 <desilva-se19047@stu.kln.ac.lk>
2023-04-07 15:56:47 +0530

    push two files

---
+++ sporty-store/src/components/ProductCards.js
@@ -0,0 +1,140 @@
+import React from "react";
+import ReactStars from "react-rating-stars-component";
+import { Link, useLocation } from "react-router-dom";
+import prodcompare from "../images/prodcompare.svg";
+import wish from "../images/wish.svg";
+import wishlist from "../images/wishlist.svg";
+import volley from "../images/volley.jpg";
+import volley2 from "../images/volly-1.avif";
+import addcart from "../images/add-cart.svg";
+import view from "../images/view.svg";
+const ProductCard = (props) => {
+  const { grid } = props;
+  console.log(grid);
+  let location = useLocation();
+
+  return (
+    <>
+      <div
+        className={` ${
+          location.pathname == "/product" ? `gr-${grid}` : "col-3"
+        } `}
+      >
+        <Link
+          to={`${
+            location.pathname == "/"
+              ? "/product/:id"
+              : location.pathname == "/product/:id"
+              ? "/product/:id"
+              : ":id"
+          }`}
+          className="product-card position-relative"
+        >
+          <div className="wishlist-icon position-absolute">
+            <button className="border-0 bg-transparent">
+              <img src={wish} alt="wishlist" />
+            </button>
+          </div>
+          <div className="product-image">
+            <img src={volley} className="img-fluid" alt="product image" />
+            <img src={volley2} className="img-fluid" alt="product image" />
+          </div>
+          <div className="product-details">
+            <h6 className="brand">addidas</h6>
+            <h5 className="product-title">
+                Browse our wide selection of adidas accessories            </h5>
+            <ReactStars
+              count={5}
+              size={25}
+              value={4}
+              edit={false}
+              activeColor="#ffd700"
+            />
+            <p className={`description ${grid === 12 ? "d-block" : "d-none"}`}>
+            adidas accessories are the perfect way to complete your look, no 
+            matter the occasion. Whether you're preparing for your next soccer 
+            game, heading to yoga class, or training for an upcoming race, 
+            you'll find exactly what you need here.
+            </p>
+            <p className="price">$10.00</p>
+          </div>
+          <div className="action-bar position-absolute">
+            <div className="d-flex flex-column gap-15">
+              <button className="border-0 bg-transparent">
+                <img src={prodcompare} alt="compare" />
+              </button>
+              <button className="border-0 bg-transparent">
+                <img src={view} alt="view" />
+              </button>
+              <button className="border-0 bg-transparent">
+                <img src={addcart} alt="addcart" />
+              </button>
+            </div>
+          </div>
+        </Link>
+      </div>
+      <div
+        className={` ${
+          location.pathname == "/product" ? `gr-${grid}` : "col-3"
+        } `}
+      >
+        <Link
+          to={`${
+            location.pathname == "/"
+              ? "/product/:id"
+              : location.pathname == "/product/:id"
+              ? "/product/:id"
+              : ":id"
+          }`}
+          className="product-card position-relative"
+        >
+          <div className="wishlist-icon position-absolute">
+            <button className="border-0 bg-transparent">
+              <img src={wish} alt="wishlist" />
+            </button>
+          </div>
+          <div className="product-image">
+            <img src={volley} className="img-fluid" alt="product image" />
+            <img src={volley2} className="img-fluid" alt="product image" />
+          </div>
+          <div className="product-details">
+            <h6 className="brand">Nike</h6>
+            <h5 className="product-title">
+            Nike produces sporting equipment
+            </h5>
+            <ReactStars
+              count={5}
+              size={24}
+              value={4}
+              edit={false}
+              activeColor="#ffd700"
+            />
+            <p className={`description ${grid === 12 ? "d-block" : "d-none"}`}>
+            It designs athletic, leisure, and casual footwear for men, women, 
+            and children. The company 's footwear products include running, 
+            training, basketball, soccer, sport-inspired urban shoes, 
+            and children 's shoes. The Jordan brand is its most popular 
+            endorsement basketball shoe.
+            </p>
+            <p className="price">$100.00</p>
+          </div>
+          <div className="action-bar position-absolute">
+            <div className="d-flex flex-column gap-15">
+              <button className="border-0 bg-transparent">
+                <img src={prodcompare} alt="compare" />
+              </button>
+              <button className="border-0 bg-transparent">
+                <img src={view} alt="view" />
+              </button>
+              <button className="border-0 bg-transparent">
+                <img src={addcart} alt="addcart" />
+              </button>
+            </div>
+          </div>
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default ProductCard;
