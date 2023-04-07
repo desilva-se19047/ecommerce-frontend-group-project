25de62438ff4b8001eec5577102f6778c3f4f7f1
desilva-se19047 <desilva-se19047@stu.kln.ac.lk>
2023-04-07 22:48:01 +0530

    push files from rajeeth branch

---
+++ sporty-store admin/src/components/MainLayout.js
@@ -0,0 +1,198 @@
+import React, { useState } from "react";
+import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
+import {
+  AiOutlineDashboard,
+  AiOutlineShoppingCart,
+  AiOutlineUser,
+  AiOutlineBgColors,
+} from "react-icons/ai";
+import {
+  LogoutOutlined,
+} from '@ant-design/icons';
+import logo from './logo.png';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import { FaClipboardList } from "react-icons/fa";
+import { SiBrandfolder } from "react-icons/si";
+import { BiCategoryAlt} from "react-icons/bi";
+import { Layout, Menu, theme } from "antd";
+import { useNavigate } from "react-router-dom";
+import './layout.css';
+
+const { Header, Sider, Content } = Layout;
+const MainLayout = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+  const navigate = useNavigate();
+  return (
+    <Layout /* onContextMenu={(e) => e.preventDefault()} */>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="logo">
+        <img src={logo} alt="logo" className="logo"/>
+        <h3 className="lg-logo">SPORTSHOP</h3>
+        </div>
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={[""]}
+          onClick={({ key }) => {
+            if (key == "signout") {
+              navigate('/')
+            } else {
+              navigate(key);
+            }
+          }}
+          items={[
+            {
+              key: "",
+              icon: <AiOutlineDashboard className="fs-4" />,
+              label: "Dashboard",
+            },
+            {
+              key: "customers",
+              icon: <AiOutlineUser className="fs-4" />,
+              label: "Customers",
+            },
+            {
+              key: "Catalog",
+              icon: <AiOutlineShoppingCart className="fs-4" />,
+              label: "Catalog",
+              children: [
+                {
+                  key: "product",
+                  icon: <AiOutlineShoppingCart className="fs-4" />,
+                  label: "Add Product",
+                },
+                {
+                  key: "list-product",
+                  icon: <AiOutlineShoppingCart className="fs-4" />,
+                  label: "Product List",
+                },
+                {
+                  key: "brand",
+                  icon: <SiBrandfolder className="fs-4" />,
+                  label: "Brand",
+                },
+                {
+                  key: "list-brand",
+                  icon: <SiBrandfolder className="fs-4" />,
+                  label: "Brand List ",
+                },
+                {
+                  key: "category",
+                  icon: <BiCategoryAlt className="fs-4" />,
+                  label: "Category",
+                },
+                {
+                  key: "list-category",
+                  icon: <BiCategoryAlt className="fs-4" />,
+                  label: "Category List",
+                },
+                {
+                  key: "color",
+                  icon: <AiOutlineBgColors className="fs-4" />,
+                  label: "Color",
+                },
+                {
+                  key: "list-color",
+                  icon: <AiOutlineBgColors className="fs-4" />,
+                  label: "Color List",
+                },
+              ],
+            },
+            {
+              key: "orders",
+              icon: <FaClipboardList className="fs-4" />,
+              label: "Orders",
+            },
+            {
+              key: "signout",
+              icon: <LogoutOutlined className="fs-4" />,
+              label: "Sign Out",
+              
+            },
+          ]}
+        >
+        </Menu>
+      </Sider>
+      <Layout className="site-layout">
+        <Header
+          className="d-flex justify-content-between ps-1 pe-5"
+          style={{
+            padding: 0,
+            background: colorBgContainer,
+          }}
+        >
+          {React.createElement(
+            collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+            {
+              className: "trigger",
+              onClick: () => setCollapsed(!collapsed),
+            }
+          )}
+          <div className="d-flex gap-4 align-items-center">
+
+            <div className="d-flex gap-3 align-items-center dropdown">
+            <div>
+                
+              </div>
+              <div
+                role="button"
+                id="dropdownMenuLink"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              ><img
+              width={35}
+              height={35}
+              src="https://stroyka-admin.html.themeforest.scompiler.ru/variants/ltr/images/customers/customer-4-64x64.jpg"
+              alt=""
+              className="admin-profile"
+            />
+                
+              </div>
+              <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+               <h5 className="dropdown-item py-1 mb-1">Name: Rajeeth</h5>
+               <p className="dropdown-item py-1 mb-1">Email: rajeeth1999@gmail.com</p>
+                <li>
+                  <Link
+                    className="dropdown-item py-1 mb-1"
+                    style={{ height: "auto", lineHeight: "20px" }}
+                    to="/"
+                  >
+                    Signout
+                  </Link>
+                </li>
+              </div>
+            </div>
+          </div>
+        </Header>
+        <Content
+          style={{
+            margin: "24px 16px",
+            padding: 24,
+            minHeight: 280,
+            background: colorBgContainer,
+          }}
+        >
+          <ToastContainer
+            position="top-right"
+            autoClose={250}
+            hideProgressBar={false}
+            newestOnTop={true}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            theme="light"
+          />
+          <Outlet />
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
+export default MainLayout;
