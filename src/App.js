5de5c5720f9220673db3a91e5b75d722eb38a50d
RavinduKavinda <dlravindukavinda@gmail.com>
2023-04-07 17:47:36 +0530

    redux persist add

--- src/App.js
+++ src/App.js
@@ -9,9 +9,11 @@ import {
   Redirect
 } from "react-router-dom";
 import Success from './pages/Success';
+import { useSelector } from 'react-redux';
 
 
 const App = () => {
+  const user = useSelector(state => state.user.currentUser);
   return (
     <Router>
       <Routes>
--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import {store,persistor} from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,7 +16,9 @@ root.render(
 
 ReactDOM.render(
   <Provider store={store}>
+  <PersistGate loading ={null} persistor ={persistor}>
     <App/>
+  </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
