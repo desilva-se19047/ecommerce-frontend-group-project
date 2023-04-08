2f2aa2887bf0269f50b286d6f70068f929c7e72f
desilva-se19047 <desilva-se19047@stu.kln.ac.lk>
2023-04-08 09:46:41 +0530

    change package json and package-lock json file wen again install npm

--- sporty-store/src/App.js
+++ sporty-store/src/App.js
@@ -21,12 +21,28 @@ import TermAndContions from "./pages/TermAndContions";
 import SingleProduct from "./pages/SingleProduct";
 import Cart from "./pages/Cart";
 import Checkout from "./pages/Checkout";
+import Dashboard from "./pages/Dashboard";
+import MainLayout from "./components/MainLayout";
+import Orders from "./pages/Orders";
+import Customers from "./pages/Customers";
+import Colorlist from "./pages/Colotlist";
+import Categorylist from "./pages/Categorylist";
+import Brandlist from "./pages/Brandlist";
+import Productlist from "./pages/Productlist";
+import Addcolor from "./pages/Addcolor";
+import Addcat from "./pages/Addcat";
+import Addbrand from "./pages/Addbrand";
+import Addproduct from "./pages/Addproduct";
+import ViewOrder from "./pages/ViewOrder";
+
 
 function App() {
   return (
     <>
       <BrowserRouter>
+
         <Routes>
+
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
             <Route path="about" element={<About />} />
@@ -48,7 +64,30 @@ function App() {
             <Route path="shipping-policy" element={<ShippingPolicy />} />
             <Route path="term-conditions" element={<TermAndContions />} />
           </Route>
+
+          <Route path="/" element={<Login />} />
+            <Route path="/reset-password" element={<Resetpassword />} />
+            <Route path="/forgot-password" element={<Forgotpassword />} />
+            <Route path="/admin" element={<MainLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="orders" element={<Orders />} />
+            <Route path="order/:id" element={<ViewOrder />} />
+            <Route path="customers" element={<Customers />} />
+            <Route path="list-color" element={<Colorlist />} />
+            <Route path="color" element={<Addcolor />} />
+            <Route path="color/:id" element={<Addcolor />} />
+            <Route path="list-category" element={<Categorylist />} />
+            <Route path="category" element={<Addcat />} />
+            <Route path="category/:id" element={<Addcat />} />
+            <Route path="list-brand" element={<Brandlist />} />
+            <Route path="brand" element={<Addbrand />} />
+            <Route path="brand/:id" element={<Addbrand />} />
+            <Route path="list-product" element={<Productlist />} />
+            <Route path="product" element={<Addproduct />} />
+          </Route>
+
         </Routes>
+        
       </BrowserRouter>
     </>
   );
