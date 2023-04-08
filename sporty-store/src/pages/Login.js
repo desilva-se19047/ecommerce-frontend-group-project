af48f9464904aa052ad54e2cd4589f9bc2abfe27
desilva-se19047 <desilva-se19047@stu.kln.ac.lk>
2023-04-08 08:34:10 +0530

    modified

--- sporty-store/src/pages/Login.js
+++ sporty-store/src/pages/Login.js
@@ -1,11 +1,47 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import * as yup from "yup";
+import { useFormik } from "formik";
+import { useDispatch, useSelector } from "react-redux";
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
 import Container from "../components/Container";
 import CustomInput from "../components/CustomInput";
+import { login } from "../features/auth/authSlice";
+
+let schema = yup.object().shape({
+  email: yup
+    .string()
+    .email("Email should be valid")
+    .required("Email is Required"),
+  password: yup.string().required("Password is Required"),
+});
 
 const Login = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: schema,
+    onSubmit: (values) => {
+      dispatch(login(values));
+    },
+  });
+  const authState = useSelector((state) => state);
+
+  const { user, isError, isSuccess, isLoading, message } = authState.auth;
+
+  useEffect(() => {
+    if (isSuccess) {
+      navigate("admin");
+    } else {
+      navigate("");
+    }
+  }, [user, isError, isSuccess, isLoading]);
+
   return (
     <>
       <Meta title={"Login"} />
@@ -16,24 +52,49 @@ const Login = () => {
           <div className="col-12">
             <div className="auth-card">
               <h3 className="text-center mb-3">Login</h3>
-              <form action="" className="d-flex flex-column gap-15">
-                <CustomInput type="email" name="email" placeholder="Email" />
+              <form action="" onSubmit={formik.handleSubmit} className="d-flex flex-column gap-15">
+                <CustomInput
+                  type="text"
+                  label="Email Address"
+                  id="email"
+                  name="email"
+                  onChng={formik.handleChange("email")}
+                  onBlr={formik.handleBlur("email")}
+                  val={formik.values.email}
+                />
+                <div className="error mt-2">
+                  {formik.touched.email && formik.errors.email}
+                </div>
                 <CustomInput
                   type="password"
+                  label="Password"
+                  id="pass"
                   name="password"
-                  placeholder="Password"
+                  onChng={formik.handleChange("password")}
+                  onBlr={formik.handleBlur("password")}
+                  val={formik.values.password}
                 />
-                <div>
-                  <Link to="/forgot-password">Forgot Password?</Link>
-
-                  <div className="mt-3 d-flex justify-content-center gap-15 align-items-center">
-                    <button className="button border-0" type="submit">
-                      Login
-                    </button>
-                    <Link to="/signup" className="button signup">
-                      SignUp
-                    </Link>
-                  </div>
+                <div className="error mt-2">
+                  {formik.touched.password && formik.errors.password}
+                </div>
+                <div className="mb-3 text-end">
+                  <Link to="forgot-password" className="">
+                    Forgot Password?
+                  </Link>
+                </div>
+                <div className="mt-3 d-flex justify-content-center gap-15 align-items-center">
+                  <button
+                    className="button border-0"
+                    type="submit"
+                  >
+                    Login
+                  </button>
+                  <Link to="/signup" className="button signup">
+                    SignUp
+                  </Link>
+                </div>
+                <div className="error text-center mt-3">
+                  {message.message == "Rejected" ? "You are not an Admin" : ""}
                 </div>
               </form>
             </div>
